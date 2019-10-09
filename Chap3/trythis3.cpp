39d6cdb38de495c933fac95487ff3a64187917b8
Jake Barrington <mjakebarrington@gmail.com>
2019-10-09 13:25:03 -0400

    Added word count to third exercise

--- Chap3/trythis3.cpp
+++ Chap3/trythis3.cpp
@@ -9,11 +9,13 @@ using namespace std;
 
 int main()
 {
+	int word_count = 0;
 	string previous = " ";
 	string current;
 	while (cin>>current) {
+		++word_count;
 		if(previous == current)
-			cout << "Repeated word = " << current << endl;
+			cout << "\nRepeated word: " << current << " found at position " << word_count << endl;
 		previous = current;
 	}
 	return 0;
